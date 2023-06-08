5a79a07a15d7a7786177db225c97d91e17b07e09
Andrew Ross <aross9913@gmail.com>
2023-06-08 23:03:05 +0000

    Justin gave me alot of help

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
 
+var currentpage=0;
+const pages = {
 
-const pages ={
-        
        1: "I can read your mind",
        2: "pick a number between 1 and 99",
        3: "add both digits together to get a new number",
        4: "subtract your new number from the original number",
-       5: "0-&, 1-@ ,2-$, 3-B",
-       6:"Its & dummy."
-        
-        
-   
+       5:" ",  //random symbols
+       6: "its this dummy"
+
 }
-var i = pages[0];
-function next_slide()
+let symbols = ["&","@","$","B","A","%", ""];
+let currentsymbol = "";
+//currentsymbol
+//
+function next_slide() {
+     // increment currentpage by 1
+     // invoke the render function
+     currentpage++;
+     render();
+}
+function restart() {
+       // reset currentpage to 1
+       //invoke render function
+       currentpage=1;
+       render();
+}
+function render()
 {
+     //display the 
+       document.getElementById("display").textContent=pages[currentpage];
+       //read from state and create the view
+       //assigns the text content
+      //if currentpage =5 ; randomize the symbols when you first goto page
+    
+      {
 
-console.log("click");
-
-
-}
-function restart()
+      }
+}      
+function generateSymbols()
 {
-console.log("click");
-
+       //set chosen symbol from the array randomly
+       //look through 0-100
+       //concatenate a string or create elements to pin back to the DOM
+       //set pages[5] to string or a placeholder
+       //replace all blank spaces with symbols in array
+       //set pages[6] to 'whatever dummy' concatenation of chosen symbol and the string
 }
-
