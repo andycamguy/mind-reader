a97aedad45278bd4bf8045993e9df95a2f9ee2cf
Andrew Ross <aross9913@gmail.com>
2023-06-08 18:49:01 +0000

    updating

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
 const card ={
-  Title: " ",
-  // button for go/restart
-  button: document.getElementById("go-button")
-   handleClickgo: function(this.pages) {
-    // goto card 2 or start over
-     if this.pages = pages[0]
-     {
-      return pages++;
-     }
-     else
-     {
-       
-
-  },
-
-  init: function() {
-    this.button.addEventListener('click', this.handleClickgo);
-    //when not go become restart
-  }
-  
-  init: function() {
-    this.button.addEventListener('click', this.handleClick);
-    //if current stage is page 1, no next button
-    //when current stage is page 5, change text to reveal
-    
-  }
-// define the pages in the card 
-pages = 
-      {
+    pages: 
+        {
         "I can read your mind",
         "pick a number between 1 and 99",
         "add both digits together to get a new number",
@@ -35,6 +8,6 @@ pages =
         "0-&, 1-@ ,2-$, 3-B",
         "Its & dummy."
         
-      }
+       }
 }
 
