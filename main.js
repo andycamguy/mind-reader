884d3838191169b8e822c13f57c9ae7e10fcc503
Andrew Ross <aross9913@gmail.com>
2023-06-08 20:50:00 +0000

    update: made progress on the buttons

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-const card ={
-    pages: 
-        {
-        "I can read your mind",
-        "pick a number between 1 and 99",
-        "add both digits together to get a new number",
-        "subtract your new number from the original number",
-        "0-&, 1-@ ,2-$, 3-B",
-        "Its & dummy."
+
+
+const pages ={
         
-       }
+       1: "I can read your mind",
+       2: "pick a number between 1 and 99",
+       3: "add both digits together to get a new number",
+       4: "subtract your new number from the original number",
+       5: "0-&, 1-@ ,2-$, 3-B",
+       6:"Its & dummy."
+        
+        
+   
+}
+var i = pages[0];
+function next_slide()
+{
+
+console.log("click");
+
+
+}
+function restart()
+{
+console.log("click");
+
 }
 
